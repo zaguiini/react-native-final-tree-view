e309cbd9ce77a70ae7cf6dfdcc8ca73bb2bcc776
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2018-12-06 08:10:39 -0200

    initial commit

---
+++ README.md
@@ -0,0 +1,187 @@
+# react-native-tree-view
+A React Native Tree View component!
+
+## Installation
+`yarn add @zaguini/react-native-tree-view`
+or
+`npm install @zaguini/react-native-tree-view --save`
+
+## Usage
+Firstly, you have to define your data. Example:
+```js
+const family = [
+  {
+    id: 'Grandparent',
+    name: 'Grandpa',
+    age: 78,
+    children: [
+      {
+        id: 'Me',
+        name: 'Me',
+        age: 30,
+        children: [
+          {
+            id: 'Erick',
+            name: 'Erick',
+            age: 10,
+          },
+          {
+            id: 'Rose',
+            name: 'Rose',
+            age: 12,
+          },
+        ],
+      },
+    ],
+  },
+]
+```
+
+It is required that each node on the tree have its own `id` key. Obviously, it should be **unique**.
+The tree nodes are defined in the `children` key. They are an array of objects, following the same structure as the parent.
+
+After defining your data, mount the component:
+```js
+import React from 'react'
+import { Text, View } from 'react-native'
+
+import TreeView from '@zaguini/react-native-tree-view'
+
+class App extends React.PureComponent {
+  state = {
+    data: [
+      {
+        id: 'Grandparent',
+        name: 'Grandpa',
+        age: 78,
+        children: [
+          {
+            id: 'Me',
+            name: 'Me',
+            age: 30,
+            children: [
+              {
+                id: 'Erick',
+                name: 'Erick',
+                age: 10,
+              },
+              {
+                id: 'Rose',
+                name: 'Rose',
+                age: 12,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  }
+
+  componentDidMount() {
+    console.log(this.treeView.getRawData())
+  }
+
+  render() {
+    return (
+      <TreeView
+        ref={ref => this.treeView = ref}
+        data={this.state.data}
+        deleteOnLongPress
+        renderItem={(item, level) => (
+          <View>
+            <Text
+              style={{
+                marginLeft: 25 * level,
+              }}
+            >
+              {
+                item.collapsed !== null ?
+                <Text>{item.collapsed ? ' > ' : ' \\/ '}</Text> :
+                <Text> - </Text>
+              }
+              {item.name}
+            </Text>
+          </View>
+        )}
+      />
+    )
+  }
+}
+
+export default App
+```
+
+This should display:
+
+![First render](https://i.imgur.com/LWDr9Ba.png)
+
+And, after a few touches:
+
+![All expanded](https://i.imgur.com/lEWGnIW.png)
+
+## Props
+
+### `data`
+Required. The tree data to render;
+
+### `collapsedItemHeight`
+Optional. The collapsed item height. Defaults to `20`;
+
+### `idKey`
+Optional. The `id` key to refer to. Defaults to `id`;
+
+### `childrenKey`
+Optional. The `children` key to look for. Defaults to `children`;
+
+### `onItemPress`
+Optional. A callback fired when a node is pressed. The pressed node is sent as the only argument;
+
+### `onItemLongPress`
+Optional. A callback fired when a node is long pressed. The pressed node is sent as the only argument;
+
+### `deleteOnLongPress`
+Optional. Deletes the pressed node when long pressed;
+
+### `renderItem`
+**Required**. A function that must return the JSX to render the item. The arguments passed are the `child` and
+the current `level` in the tree, starting from `0`.
+You get, for free, a `collapsed` key, which could have the possible values:
+- `null` when there are no children for this node;
+- `true` when the node is collapsed;
+- `false` when the node is expanded.
+
+Example:
+
+```js
+renderItem={(item, level) => (
+  <View>
+    <Text
+      style={{
+        marginLeft: 25 * level,
+      }}
+    >
+      {
+        item.collapsed !== null ?
+        <Text>{item.collapsed ? ' > ' : ' \\/ '}</Text> :
+        <Text> - </Text>
+      }
+      {item.name}
+    </Text>
+  </View>
+)}
+```
+
+## Methods
+
+### `getRawData`
+Gets the raw, updated, tree data.
+
+## FAQ
+
+### If I modify the `data` prop, do the tree reflects the changes?
+
+Yes, it does. Feel free to modify that awesome state and see the modifications :)
+
+------
+
+License: MIT
---
+++ index.js
@@ -0,0 +1,194 @@
+import React from 'react'
+import types from 'prop-types'
+
+import {
+  TouchableOpacity,
+  View,
+} from 'react-native'
+
+class TreeView extends React.PureComponent {
+  static propTypes = {
+    data: types.array.isRequired,
+    renderItem: types.func.isRequired,
+    collapsedItemHeight: types.number,
+    idKey: types.string,
+    childrenKey: types.string,
+    onItemPress: types.func,
+    onItemLongPress: types.func,
+    deleteOnLongPress: types.bool,
+  }
+
+  static defaultProps = {
+    collapsedItemHeight: 20,
+    deleteOnLongPress: false,
+    idKey: 'id',
+    childrenKey: 'children',
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: this.makeCollapsed(props.data.slice()),
+      idKey: props.idKey,
+      childrenKey: props.childrenKey,
+    }
+
+    this.handleNodePressed = this.handleNodePressed.bind(this)
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({
+      data: this.makeCollapsed(props.data.slice()),
+    })
+  }
+
+  makeCollapsed(data) {
+    return data.map(children => {
+      if(children[this.props.childrenKey]) {
+        if(typeof children.collapsed === 'undefined') {
+          children.collapsed = true
+        }
+
+        this.makeCollapsed(children[this.props.childrenKey])
+      } else {
+        children.collapsed = null
+      }
+
+      return children
+    })
+  }
+
+  toggleCollapse(data, id) {
+    let found
+
+    return data.map(children => {
+      if(
+        children[this.props.idKey] === id &&
+        typeof children.collapsed !== 'undefined' &&
+        children.collapsed !== null
+      ) {
+        children.collapsed = !children.collapsed
+        found = true
+      }
+
+      if(!found && children[this.props.childrenKey]) {
+        this.toggleCollapse(children[this.props.childrenKey], id)
+      }
+
+      return children
+    })
+  }
+
+  removeNull(data) {
+    data.map((children, i) => {
+      if(children[this.props.childrenKey]) {
+        children[this.props.childrenKey] = children[this.props.childrenKey]
+          .filter(i => i !== null)
+
+        if(!children[this.props.childrenKey].length) {
+          delete children[this.props.childrenKey]
+          children.collapsed = null
+        }
+      }
+    })
+  }
+
+  deleteNode(data, id) {
+    data.map((children, i) => {
+      if(children[this.props.childrenKey]) {
+        this.deleteNode(children[this.props.childrenKey], id)
+      }
+
+      if(children[this.props.idKey] === id) {
+        delete data[i]
+      }
+    })
+
+    this.removeNull(data)
+
+    return data
+  }
+
+  removeCollapsedKey(data) {
+    data.map((children, i) => {
+      if(children[this.props.childrenKey]) {
+        this.removeCollapsedKey(children[this.props.childrenKey])
+      }
+
+      delete children.collapsed
+    })
+
+    return data
+  }
+
+  getRawData() {
+    return this.removeCollapsedKey(this.state.data.slice())
+  }
+
+  handleNodePressed(children) {
+    const newData = this.toggleCollapse(
+      this.state.data.slice(),
+      children[this.props.idKey]
+    )
+
+    this.setState({
+      data: newData,
+    })
+
+    this.props.onItemPress && this.props.onItemPress(children)
+  }
+
+  handleDeleteNode(id) {
+    const data = this.deleteNode(this.state.data.slice(), id)
+
+    this.setState({
+      data,
+    })
+  }
+
+  renderTree(data, level) {
+    return data.map(children => {
+      const hasChildren = children[this.props.childrenKey] &&
+        children[this.props.childrenKey].length > 0
+
+      return (
+        <View
+          key={children[this.props.idKey]}
+          style={{
+            height: (
+              children.collapsed ? this.props.collapsedItemHeight : 'auto'
+            ),
+            zIndex: 1,
+            overflow: 'hidden',
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => this.handleNodePressed(children)}
+            onLongPress={() => {
+              if(this.props.onItemLongPress) {
+                this.props.onItemLongPress(children)
+              }
+
+              if(this.props.deleteOnLongPress) {
+                this.handleDeleteNode(children[this.props.idKey])
+              }
+            }}
+          >
+            {this.props.renderItem(children, level)}
+          </TouchableOpacity>
+          {
+            hasChildren &&
+            this.renderTree(children[this.props.childrenKey], level + 1)
+          }
+        </View>
+      )
+    })
+  }
+
+  render() {
+    return this.renderTree(this.state.data, 0)
+  }
+}
+
+export default TreeView
