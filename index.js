f0dab24c6df3b645f9ac6e5a9cd2b23a036e85a1
Luis Felipe Zaguini <zaguini@Zaguinis-Macbook-Pro.local>
2019-08-14 21:04:20 -0300

    refactored tree view

--- index.js
+++ index.js
@@ -1,192 +1,121 @@
+import get from 'lodash.get'
+import PropTypes from 'prop-types'
 import React from 'react'
-import types from 'prop-types'
+import { TouchableOpacity, View } from 'react-native'
 
-import {
-  TouchableOpacity,
-  View,
-} from 'react-native'
+function noop() {}
 
-class TreeView extends React.PureComponent {
+class TreeView extends React.Component {
   static propTypes = {
-    data: types.array.isRequired,
-    renderItem: types.func.isRequired,
-    collapsedItemHeightForLevel: types.func,
-    idKey: types.string,
-    childrenKey: types.string,
-    onItemPress: types.func,
-    onItemLongPress: types.func,
-    deleteOnLongPress: types.bool,
+    data: PropTypes.array.isRequired,
+    renderNode: PropTypes.func.isRequired,
+    getCollapsedNodeHeight: PropTypes.func,
+    idKey: PropTypes.string,
+    childrenKey: PropTypes.string,
+    onNodePress: PropTypes.func,
+    onNodeLongPress: PropTypes.func,
   }
 
   static defaultProps = {
-    collapsedItemHeightForLevel: (level) => {
-      return 20
-    },
-    deleteOnLongPress: false,
+    initialExpanded: false,
+    getCollapsedNodeHeight: () => 20,
     idKey: 'id',
     childrenKey: 'children',
+    onNodePress: noop,
+    onNodeLongPress: noop,
   }
 
   constructor(props) {
     super(props)
 
-    this.state = {
-      data: this.makeCollapsed(props.data.slice()),
-      idKey: props.idKey,
-      childrenKey: props.childrenKey,
+    this.state = this.getInitialState()
+  }
+
+  getInitialState = () => ({
+    expandedNodeKeys: {},
+  })
+
+  componentDidUpdate(prevProps) {
+    const hasDataUpdated = prevProps.data !== this.props.data
+    const hasIdKeyUpdated = prevProps.idKey !== this.props.idKey
+    const childrenKeyUpdated = prevProps.childrenKey !== this.props.childrenKey
+
+    if (hasDataUpdated || hasIdKeyUpdated || childrenKeyUpdated) {
+      this.setState(this.getInitialState())
     }
-
-    this.handleNodePressed = this.handleNodePressed.bind(this)
   }
 
-  componentWillReceiveProps(props) {
-    this.setState({
-      data: this.makeCollapsed(props.data.slice()),
-    })
+  hasChildrenNodes = (node) =>
+    get(node, `${this.props.childrenKey}.length`, 0) > 0
+
+  isExpanded = (id) =>
+    get(this.state.expandedNodeKeys, id, this.props.initialExpanded)
+
+  expandNodeKeyById = (id, expanded) => ({ expandedNodeKeys }) => ({
+    expandedNodeKeys: Object.assign({}, expandedNodeKeys, {
+      [id]: expanded,
+    }),
+  })
+
+  collapseNode = (id) => this.setState(this.expandNodeKeyById(id, false))
+
+  expandNode = (id) => this.setState(this.expandNodeKeyById(id, true))
+
+  toggleCollapse = (id) => {
+    const method = this.isExpanded(id) ? 'collapseNode' : 'expandNode'
+
+    this[method](id)
   }
 
-  makeCollapsed(data) {
-    return data.map(children => {
-      if(children[this.props.childrenKey]) {
-        if(typeof children.collapsed === 'undefined') {
-          children.collapsed = true
-        }
+  handleNodePressed = async ({ node, level }) => {
+    const nodePressResult = await this.props.onNodePress({ node, level })
 
-        this.makeCollapsed(children[this.props.childrenKey])
-      } else {
-        children.collapsed = null
-      }
-
-      return children
-    })
+    if (nodePressResult !== false && this.hasChildrenNodes(node)) {
+      this.toggleCollapse(node[this.props.idKey])
+    }
   }
 
-  toggleCollapse(data, id) {
-    let found
+  Tree = ({ nodes, level }) => {
+    return nodes.map((node) => {
+      const isExpanded = this.isExpanded(node[this.props.idKey])
+      const hasChildrenNodes = this.hasChildrenNodes(node)
+      const shouldRenderLevel = hasChildrenNodes && isExpanded
 
-    return data.map(children => {
-      if(
-        children[this.props.idKey] === id &&
-        typeof children.collapsed !== 'undefined' &&
-        children.collapsed !== null
-      ) {
-        children.collapsed = !children.collapsed
-        found = true
-      }
-
-      if(!found && children[this.props.childrenKey]) {
-        this.toggleCollapse(children[this.props.childrenKey], id)
-      }
-
-      return children
-    })
-  }
-
-  removeNull(data) {
-    data.map((children, i) => {
-      if(children[this.props.childrenKey]) {
-        children[this.props.childrenKey] = children[this.props.childrenKey]
-          .filter(i => i !== null)
-
-        if(!children[this.props.childrenKey].length) {
-          delete children[this.props.childrenKey]
-          children.collapsed = null
-        }
-      }
-    })
-  }
-
-  deleteNode(data, id) {
-    data.map((children, i) => {
-      if(children[this.props.childrenKey]) {
-        this.deleteNode(children[this.props.childrenKey], id)
-      }
-
-      if(children[this.props.idKey] === id) {
-        delete data[i]
-      }
-    })
-
-    this.removeNull(data)
-
-    return data
-  }
-
-  removeCollapsedKey(data) {
-    data.map((children, i) => {
-      if(children[this.props.childrenKey]) {
-        this.removeCollapsedKey(children[this.props.childrenKey])
-      }
-
-      delete children.collapsed
-    })
-
-    return data
-  }
-
-  getRawData() {
-    return this.removeCollapsedKey(this.state.data.slice())
-  }
-
-  handleNodePressed(children, level) {
-    const newData = this.toggleCollapse(
-      this.state.data.slice(),
-      children[this.props.idKey]
-    )
-
-    this.setState({
-      data: newData,
-    })
-
-    this.props.onItemPress && this.props.onItemPress(children, level)
-  }
-
-  handleDeleteNode(id) {
-    const data = this.deleteNode(this.state.data.slice(), id)
-
-    this.setState({
-      data,
-    })
-  }
-
-  renderTree(data, level) {
-    return data.map(children => {
-      const hasChildren = children[this.props.childrenKey] &&
-        children[this.props.childrenKey].length > 0
       return (
         <View
-          key={children[this.props.idKey]}
+          key={node[this.props.idKey]}
           style={{
-            height: (
-              children.collapsed ? this.props.collapsedItemHeightForLevel(level) : 'auto'
-            ),
+            height: isExpanded
+              ? 'auto'
+              : this.props.getCollapsedNodeHeight({
+                  [this.props.idKey]: node[this.props.idKey],
+                  level,
+                }),
             zIndex: 1,
             overflow: 'hidden',
           }}
         >
           <TouchableOpacity
-            onPress={() => this.handleNodePressed(children, level)}
-            onLongPress={() => {
-              if(this.props.onItemLongPress) {
-                this.props.onItemLongPress(children, level)
-              } else if(this.props.deleteOnLongPress) {
-                this.handleDeleteNode(children[this.props.idKey])
-              }
-            }}
+            onPress={() => this.handleNodePressed({ node, level })}
+            onLongPress={() => this.props.onNodeLongPress({ node, level })}
           >
-            {this.props.renderItem(children, level)}
+            {React.createElement(this.props.renderNode, {
+              node,
+              level,
+              isExpanded,
+              hasChildrenNodes,
+            })}
           </TouchableOpacity>
-          {
-            hasChildren &&
-            this.renderTree(children[this.props.childrenKey], level + 1)
-          }
+          {shouldRenderLevel && (
+            <this.Tree nodes={node[this.props.childrenKey]} level={level + 1} />
+          )}
         </View>
       )
     })
   }
 
   render() {
-    return this.renderTree(this.state.data, 0)
+    return <this.Tree nodes={this.props.data} level={0} />
   }
 }
 
